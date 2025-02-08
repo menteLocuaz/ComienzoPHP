27763eebb06c14a7c65d59ed597c49ff89f5c499
MenteLocuaz <emersondurate.45@gmail.com>
2025-02-08 11:12:18 -0600

    Inicio con php

---
+++ Inicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Master en PHP</title>
+</head>
+
+<body>
+    <h1>Master en PHP <?php echo "Desde la CUN" ?> </h1>
+
+    <?php
+    //  Comentario de codigo 
+    echo "Bienvenido al curso mas grand de PHP";
+    // . El punto sirve para unir caracteres 
+    echo "<h3>Lista de video juegos </h3>";
+    echo "<ul>"
+        . "<li>GTA</li>"
+        . "<li>FIFA </li>"
+        . "<li>Mario Bros </li>"
+        . "</ul>";
+
+    echo "<p>Esta es todo" . " - " . "lista de juegos</p>";
+    ?>
+</body>
+
+</html>
