bc1b325caee5be67517bc66bb8be6ba61b095d51
MenteLocuaz <emersondurate.45@gmail.com>
2025-02-08 16:06:53 -0600

    operadores y asinagcion

---
+++ 06-operadores/operadores.php
@@ -0,0 +1,19 @@
+<?php
+// opreadores arimetico
+$numero1 =5;
+$numero2 = 5;
+
+echo 'Suma :'.($numero1+ $numero2).'<br/>';
+echo 'Resta :'.($numero1-$numero2).'<br/>';
+echo 'Multiplicacion :'.($numero1*$numero2).'<br/>';
+
+// operadores de incremento y decremento 
+$yaer = 2019;
+echo '<h1>Fecha :'.$yaer.'</h1>';
+echo '<h1>Incremento </h1>';
+// incremento ++ ,decrementon -- 
+$yaer++;
+echo '<h1>Fecha :'.$yaer.'</h1>';
+// operador de asignacion 
+echo $yaer +=5;
+?>
