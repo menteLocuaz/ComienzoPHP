f831122321e9660e5f0912266e100b51a1ea911b
MenteLocuaz <emersondurate.45@gmail.com>
2025-02-08 13:39:35 -0600

    Tipo de datos

---
+++ 04-TipoDeDato/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Tipo de Datos
+Entero (int/interger) = 99
+Flotanter (float/double) = 3.45
+cadena string = "soy una cadena "
+Boleano (boll) tru/false
+*/
+
+// Como saber que tipo de dato es
+$valro = 44;$rublo = 4.55;$nula = null;
+echo "<h1>Entero</h1>";
+echo gettype($valro);
+echo "<h1>Flotante</h1>";
+echo gettype($rublo);
+
+// curisiodades, debe de decir null ya que beta error
+// Debugear 
+echo "</br>";
+$mi_nombre = 'Vamos campeon';
+var_dump($mi_nombre);
+
+?>
