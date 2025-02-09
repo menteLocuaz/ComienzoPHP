26297f5d962a35db1210e35fc686268c195b24f0
MenteLocuaz <emersondurate.45@gmail.com>
2025-02-08 18:22:56 -0600

    condion IF

---
+++ 08-condicionIF/index.php
@@ -0,0 +1,17 @@
+<?php
+// condicion
+//  == igual
+// <> diferente
+// < menor que
+// > mayor que
+// <= meno r o igual
+// >= mayor  o igual 
+$color = 'rojo';
+
+if ($rojo == 'rojo') {
+    echo 'ES COLOR ROJO';
+} else {
+    echo 'No es color ';
+};
+
+?>
